bc28b76b0ddfc62db4a287e5f51071248d63516a
Levan Velijanashvili <stichoza@gmail.com>
2020-03-29 19:40:50 +0400

    Working version

--- src/Command.php
+++ src/Command.php
@@ -6,9 +6,28 @@ use Stichoza\NbgCurrency\NbgCurrency;
 
 class Command {
 
-    public function test(string $currency): float
+    const PRECISION = 4;
+
+    const FALLBACK = 'usd';
+
+    public function run($first = null, $second = null, $third = null): void
     {
-        return (float) NbgCurrency::get($currency);
+        if (is_numeric($first)) {
+            if ($second === 'gel' || $second === 'to') {
+                echo round($first / $this->rate($third ?? self::FALLBACK), self::PRECISION);
+            } else {
+                echo $first * $this->rate($second ?? self::FALLBACK);
+            }
+        } else {
+            echo $this->rate($first ?: self::FALLBACK);
+        }
+
+        echo PHP_EOL;
+    }
+
+    public function rate($currency): float
+    {
+        return (float) NbgCurrency::rate($currency);
     }
 
 }
