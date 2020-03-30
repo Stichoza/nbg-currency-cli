d171f4c601f0d22f8fa4d28c9c582221f7faacc7
Levan Velijanashvili <stichoza@gmail.com>
2020-03-30 05:26:57 +0400

    Change all colors

--- src/Command.php
+++ src/Command.php
@@ -24,13 +24,13 @@ class Command {
     public function run(): void
     {
         if ($this->hasOption('help')) {
-            echo Color::bold(), "NBG Currency CLI ", Color::reset(), Color::light_green(), "by Stichoza\n", Color::reset(),
+            echo Color::bold(), "NBG Currency CLI ", Color::light_green(), "by Stichoza\n", Color::reset(),
                 "  Command-line tool to get currency rates by National Bank of Georgia.\n\n",
-                Color::yellow(), "Options:", Color::reset(), "\n",
+                Color::light_yellow(), "Options:", Color::reset(), "\n",
                 Color::bold_green(), "  --help         ", Color::reset(), "  Display this help page.\n",
                 Color::bold_green(), "  --plain        ", Color::reset(), "  Display plain results without colors.\n",
                 Color::bold_green(), "  --normalize    ", Color::reset(), "  Convert rates to single entity if rate is given for amount larger than 1.\n\n",
-                Color::yellow(), "Example commands:", Color::reset(), "\n",
+                Color::light_yellow(), "Example commands:", Color::reset(), "\n",
                 Color::bold_green(), "  nbg usd        ", Color::reset(), "  Get currency rate and change for USD.\n",
                 Color::bold_green(), "  nbg usd --plain", Color::reset(), "  Get currency rate for USD.\n",
                 Color::bold_green(), "  nbg usd eur gbp", Color::reset(), "  Get currency rate for USD, EUR, GBP.\n",
